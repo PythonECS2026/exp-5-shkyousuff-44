ecda8255cd5bf9049d6f8816c95e1c030bdb41d0
shkyousuff-44 <shkyousuff@gmail.com>
2026-03-03 14:43:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ print(f"Original Tasks: {tasks}")
 # TODO: Remove & Print a Task selected by User
 
 # TODO: Sort & Print the Tasks
+tasks = ["Sleep", "Getup", "Brush"]
+print(f"Original Tasks: {tasks}")
+tasks.append(input())
+print(f"Tasks after Adding: {tasks}")
+edit_index = int(input())
+tasks[edit_index] = input()
+print(f"Tasks after Editing: {tasks}")
+tasks.pop(0)
+print(f"Tasks after Removing: {tasks}")
+tasks.sort()
+print(f"Tasks after Sorting: {tasks}")
